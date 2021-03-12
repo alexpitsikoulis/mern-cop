6e96e4434f94fea7a8ac7452935429f653e7a7a8
Alex Pitsikoulis <alex.pitsikoulis@gmail.com>
2021-03-11 19:23:16 -0500

    init

---
+++ client/build/precache-manifest.45c2cb5e83f2744ebf6b1d8be25245f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57883434bdf36a25a6b1034abf1cc2d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ad9111fd68abe15db03",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "5ad9111fd68abe15db03",
+    "url": "/static/js/main.b08ca56f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
